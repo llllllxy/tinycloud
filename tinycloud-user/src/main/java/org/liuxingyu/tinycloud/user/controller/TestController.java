556b91f71fc60f5c10f519f8c991eed78391f286
17862719591 <184974699@qq.com>
2023-03-18 20:11:13 +0800

    增加打印网关层传递来的header数据

--- tinycloud-user/src/main/java/org/liuxingyu/tinycloud/user/controller/TestController.java
+++ tinycloud-user/src/main/java/org/liuxingyu/tinycloud/user/controller/TestController.java
@@ -2,9 +2,13 @@ package org.liuxingyu.tinycloud.user.controller;
 
 import org.liuxingyu.tinycloud.common.base.Result;
 import org.liuxingyu.tinycloud.user.config.TinyCloudUserConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author liuxingyu01
@@ -14,13 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @Autowired
     private TinyCloudUserConfig tinyCloudUserConfig;
 
 
     @RequestMapping("/testuser")
-    public Result testuser() {
+    public Result testuser(HttpServletRequest request) {
+        log.info("userId = " + request.getHeader("USER_ID"));
+
         return Result.ok("调用成功", tinyCloudUserConfig.getName());
     }
 
