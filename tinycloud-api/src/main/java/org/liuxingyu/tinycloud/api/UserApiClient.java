629428d7533c99a6a526888121732f5db566bb21
17862719591 <184974699@qq.com>
2023-05-22 14:11:00 +0800

    更新一些依赖的版本

--- tinycloud-api/src/main/java/org/liuxingyu/tinycloud/api/UserApiClient.java
+++ tinycloud-api/src/main/java/org/liuxingyu/tinycloud/api/UserApiClient.java
@@ -6,17 +6,18 @@ import org.liuxingyu.tinycloud.bean.entity.UcUser;
 import org.liuxingyu.tinycloud.common.model.ApiResult;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
+ * 用户服务Feign接口声明
+ *
  * @author liuxingyu01
- * @date 2022-12-08 13:49
- * @description 用户服务Feign接口声明
+ * @since 2022-12-08 13:49
  **/
 @FeignClient(value = ApiServerConstants.USER_SERVER, contextId = "remoteUserService", path = "/user-server", fallbackFactory = UserApiFallbackFactory.class)
 public interface UserApiClient {
 
-    @GetMapping(value = "/api/user/detail/{userId}")
-    ApiResult<UcUser> detail(@PathVariable("userId") String userId);
+    @GetMapping(value = "/api/user/detail")
+    ApiResult<UcUser> detail(@RequestParam("userId") String userId);
 }
 
--- tinycloud-user/src/main/java/org/liuxingyu/tinycloud/user/provider/UcUserProvider.java
+++ tinycloud-user/src/main/java/org/liuxingyu/tinycloud/user/provider/UcUserProvider.java
@@ -8,10 +8,7 @@ import org.liuxingyu.tinycloud.user.service.UcUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
@@ -24,13 +21,12 @@ import java.util.Objects;
 public class UcUserProvider {
     public static final Logger logger = LoggerFactory.getLogger(UcUserProvider.class);
 
-
     @Autowired
     private UcUserService ucUserService;
 
     @ApiOperation(value = "系统用户获取", notes = "系统用户获取")
-    @GetMapping(value = "/detail/{userId}")
-    public ApiResult<UcUser> detail(@PathVariable("userId") String userId, HttpServletRequest request) {
+    @GetMapping(value = "/detail")
+    public ApiResult<UcUser> detail(@RequestParam("userId") String userId, HttpServletRequest request) {
         // 打印请求头的信息
         Enumeration<String> headerNames = request.getHeaderNames();
         if (Objects.nonNull(headerNames)) {
@@ -41,6 +37,7 @@ public class UcUserProvider {
             }
         }
 
+        // 调用service层
         UcUser ucUser = ucUserService.detail(userId);
         if (ucUser != null) {
             return ApiResult.success("查询成功!", ucUser);
