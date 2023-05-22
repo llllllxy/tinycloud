1f70e41fe433a31a4f93609883d7486380fcb221
17862719591 <184974699@qq.com>
2023-05-22 17:43:58 +0800

    改成RequestParam

--- tinycloud-message/src/main/java/org/liuxingyu/tinycloud/message/controller/MessageTestController.java
+++ tinycloud-message/src/main/java/org/liuxingyu/tinycloud/message/controller/MessageTestController.java
@@ -4,16 +4,9 @@ import org.liuxingyu.tinycloud.api.UserApiClient;
 import org.liuxingyu.tinycloud.bean.entity.UcUser;
 import org.liuxingyu.tinycloud.common.model.ApiResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * @author liuxingyu01
- * @date 2022-12-08 14:35
- * @description
- **/
+
 @RestController
 @RequestMapping("/test")
 public class MessageTestController {
@@ -21,8 +14,8 @@ public class MessageTestController {
     @Autowired
     private UserApiClient userApiClient;
 
-    @GetMapping("/detail/{userId}")
-    public ApiResult<UcUser> detail(@PathVariable("userId") String userId) {
+    @GetMapping("/detail")
+    public ApiResult<UcUser> detail(@RequestParam("userId") String userId) {
         return userApiClient.detail(userId);
     }
 }
