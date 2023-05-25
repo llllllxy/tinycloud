50d01b5793dadd53b1f17279f35401a413f6de67
17862719591 <184974699@qq.com>
2023-05-25 17:33:03 +0800

    格式化代码

--- tinycloud-common/src/main/java/org/liuxingyu/tinycloud/common/config/feign/RequestHeaderHystrixConcurrencyStrategy.java
+++ tinycloud-common/src/main/java/org/liuxingyu/tinycloud/common/config/feign/RequestHeaderHystrixConcurrencyStrategy.java
@@ -9,14 +9,21 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.concurrent.Callable;
 
-
+/**
+ * <p>
+ *  重写HystrixConcurrencyStrategy，以解决开启hystrix后，FeignRequestInterceptor无法获取到 ServletRequestAttributes的问题
+ * </p>
+ *
+ * @author liuxingyu01
+ * @since 2023-03-07 15:47:38
+ */
 @Component
 public class RequestHeaderHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
     public static final Logger logger = LoggerFactory.getLogger(RequestHeaderHystrixConcurrencyStrategy.class);
 
     @Override
-    public <T> Callable<T> wrapCallable(Callable<T> callable){
-        ServletRequestAttributes servletRequestAttributes=(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    public <T> Callable<T> wrapCallable(Callable<T> callable) {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         return new Callable<T>() {
             @Override
             public T call() throws Exception {
@@ -26,7 +33,7 @@ public class RequestHeaderHystrixConcurrencyStrategy extends HystrixConcurrencyS
                         RequestContextHolder.setRequestAttributes(servletRequestAttributes);
                     }
                     return callable.call();
-                }finally {
+                } finally {
                     RequestContextHolder.resetRequestAttributes();
                 }
             }
