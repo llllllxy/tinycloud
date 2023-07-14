95086697e1a412914a983502d66748b9c224543a
17862719591 <184974699@qq.com>
2023-07-14 09:46:13 +0800

    feat: 日志打印你切面代码更新

--- tinycloud-common/src/main/java/org/tinycloud/common/aspect/HttpRequestAspect.java
+++ tinycloud-common/src/main/java/org/tinycloud/common/aspect/HttpRequestAspect.java
@@ -2,6 +2,7 @@ package org.tinycloud.common.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.tinycloud.common.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +10,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -29,24 +34,47 @@ public class HttpRequestAspect {
      * 以 controller 包下定义的所有请求为切入点
      */
     @Pointcut("execution(* org.tinycloud..controller..*.*(..))")
-    public void reqOpenAPILog() {
+    public void controllerAPILog() {
     }
 
     /**
-     * 环绕
-     *
-     * @param proceedingJoinPoint
-     * @return
-     * @throws Throwable
+     * 以 provider 包下定义的所有请求为切入点
      */
-    @Around("reqOpenAPILog()")
+    @Pointcut("execution(* org.tinycloud..provider..*.*(..))")
+    public void providerAPILog() {
+    }
+
+    /**
+     * 环绕通知
+     */
+    @Around("controllerAPILog() || providerAPILog()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         log.info("========================================== Start ==========================================");
-        long startTime = System.currentTimeMillis();
-
         // 开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        List<Object> argList = new ArrayList<>();
+        if (args != null && args.length > 0) {
+            for (Object arg : args) {
+                if (arg instanceof ServletRequest || arg instanceof ServletResponse) {
+                    continue;
+                }
+                if (arg instanceof MultipartFile) {
+                    MultipartFile file = (MultipartFile) arg;
+                    argList.add(file.getOriginalFilename());
+                    continue;
+                }
+                argList.add(arg);
+            }
+        }
+        Object param;
+        if (argList.size() == 1) {
+            param = argList.get(0);
+        } else {
+            param = argList;
+        }
         // 打印请求 url
         log.info("Request URL    : {}", request.getRequestURL().toString());
         // 打印 Http method
@@ -55,22 +83,16 @@ public class HttpRequestAspect {
         log.info("Class Method   : {}.{}", proceedingJoinPoint.getSignature().getDeclaringTypeName(), proceedingJoinPoint.getSignature().getName());
         // 打印请求的 IP
         log.info("Request IP     : {}", request.getRemoteAddr());
+        // 打印请求入参
+        log.info("Request Args   : {}", JsonUtils.toJsonString(param));
 
-        // 打印请求入参（这是个危险操作，因为如果参数过大的话会导致日志过大） -----> 暂时去掉参数的打印
-        // Object[] args = proceedingJoinPoint.getArgs();
-        // Object param;
-        // if (args != null && args.length == 1) {
-        //    param = args[0];
-        // } else {
-        //     param = args;
-        // }
-        // log.info("Request Args   : {}", JsonUtils.toJsonString(param));
 
+        long startTime = System.currentTimeMillis();
         Object result = proceedingJoinPoint.proceed();
-        // 打印执行结果
-        log.info("Response Result : {}", JsonUtils.toJsonString(result));
+        // 打印出参
+        log.info("Response Args  : {}", JsonUtils.toJsonString(result));
         // 执行耗时
-        log.info("Time Consuming  : {} ms", System.currentTimeMillis() - startTime);
+        log.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
         log.info("=========================================== End ===========================================");
         return result;
     }
